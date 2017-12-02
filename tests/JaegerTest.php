b4e6ddef204ade5a472e5636869e24fa99c00c47
吕海涛 <i@lvht.net>
2017-12-02 21:02:12 +0800

    init phpunit

---
+++ tests/JaegerTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Jaeger;
+
+use PHPUnit\Framework\TestCase;
+
+class JaegerTest extends TestCase
+{
+    public function testFoo()
+    {
+        self::assertEquals(1, 1);
+    }
+}
